c38dae9f3bf651441881a624666bd0cbe15ec0ea
Gytis Karčiauskas <gytis@karciauskas.lt>
2014-08-14 18:36:40 +0300

    LEMP

--- raspberrypi/setup.sh
+++ raspberrypi/setup.sh
@@ -4,11 +4,30 @@
 sudo apt-get update
 
 # Remove useless packages.
-sudo apt-get remove --purge -y wolfram-engine python-picamera python3-picamera
+sudo apt-get remove --purge -y wolfram-engine python-picamera python3-picamera \
+    python-rpi.gpio python3-rpi.gpio
 
 # Upgrade software packages.
 sudo apt-get upgrade -y
 
-# Install web server.
+# Install WEB server.
 sudo apt-get install -y nginx
 sudo service nginx start
+
+# Install MySQL server with empty root password.
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -q -y mysql-server
+
+# Install PHP bridges between nginx and MySQL.
+sudo apt-get install -y php5-fpm php5-mysql
+
+# Install phpMyAdmin.
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y phpmyadmin
+
+# Create a symbolic link between phpMyAdmin and site’s directory.
+sudo ln -s /usr/share/phpmyadmin/ /usr/share/nginx/www
+
+# Change default site configuration.
+wget https://raw.githubusercontent.com/gyKa/setup/master/raspberrypi/etc/nginx/sites-available/default
+sudo mv default /etc/nginx/sites-available/
+sudo service php5-fpm restart
+sudo service nginx restart

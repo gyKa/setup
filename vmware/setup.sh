5405b97db2226efaf08a4511f7d59982b4f6ada9
Gytis Karčiauskas <gytis@karciauskas.lt>
2014-11-03 21:41:50 +0200

    Install Bottle framework.

--- vmware/setup.sh
+++ vmware/setup.sh
@@ -52,3 +52,9 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
+
+echo "===> Installing PyPI..."
+sudo apt-get install -y python-pip
+
+echo "===> Installing Bottle framework..."
+sudo pip install bottle

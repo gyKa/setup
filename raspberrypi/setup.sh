b826bfecd3d649c01b8868e0276f8f3be0397c20
Gytis Karčiauskas <gytis@karciauskas.lt>
2015-01-13 06:26:19 +0200

    Append old version.

--- raspberrypi/setup.sh
+++ raspberrypi/setup.sh
@@ -9,61 +9,64 @@ sudo rm -rf ~/python_games/ ~/ocr_pi.png ~/Desktop/
 # Remove useless packages.
 sudo apt-get remove --purge -y wolfram-engine python-picamera python3-picamera \
     python-rpi.gpio python3-rpi.gpio gnome-themes-standard-data \ 
-    gnome-icon-theme zenity zenity-common
+    gnome-icon-theme zenity zenity-common xserver-xorg-core xserver-common
 
 sudo apt-get autoremove --purge -y
 
 # Upgrade software packages.
 sudo apt-get upgrade -y
 
-# Install IRC client and screen.
-sudo apt-get install -y weechat screen
+# Install system utils.
+sudo apt-get install -y htop vim
 
-# Install WEB server.
+# Install IRC client and screen.
+sudo apt-get install -y weechat weechat-scripts screen
+
+# Autoload Weechat scripts.
+ln -s /usr/share/weechat/perl/buffers.pl ~/.weechat/perl/autoload/buffers.pl
+
+# Install NodeJS and NPM.
+wget http://nodejs.org/dist/v0.10.2/node-v0.10.2-linux-arm-pi.tar.gz
+tar -xvzf node-v0.10.2-linux-arm-pi.tar.gz
+echo "NODE_JS_HOME=/home/pi/node-v0.10.2-linux-arm-pi" >> ~/.bash_profile
+echo "PATH=$PATH:$NODE_JS_HOME/bin" >> ~/.bash_profile
+source ~/.bash_profile
+rm node-v0.10.2-linux-arm-pi.tar.gz
+
+# Install Hubot.
+sudo npm install -g hubot coffee-script
+
+# Install Redis.
+wget http://download.redis.io/releases/redis-2.8.17.tar.gz
+tar xzf redis-2.8.17.tar.gz
+cd redis-2.8.17
+make # take cup of coffe
+make test
+sudo make install
+cd utils
+sudo ./install_server.sh
+cd ../..
+rm redis-2.8.17.tar.gz
+
+# Install Nginx.
 sudo apt-get install -y nginx
 sudo service nginx start
 
-# Install MySQL server with empty root password.
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -q -y mysql-server
+# Install PHP.
+sudo apt-get install -y php5-fpm
 
-# Install PHP bridges between nginx and MySQL.
-sudo apt-get install -y php5-fpm php5-mysql
-
-# Install PHP command line interface.
-sudo apt-get install -y php5-cli
-
-# Install phpMyAdmin.
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y phpmyadmin
-
-# Create a symbolic link between phpMyAdmin and site’s directory.
-sudo ln -s /usr/share/phpmyadmin/ /usr/share/nginx/www
-
-# Change default site configuration.
-wget https://raw.githubusercontent.com/gyKa/setup/master/raspberrypi/etc/nginx/sites-available/default
+# Setup Nginx and PHP.
+sudo mkdir -p /var/www
+sudo chown -R $USER:$USER /var/www
+sudo chmod -R 755 /var/www
+wget https://raw.githubusercontent.com/gyKa/setup/master/raspberrypi/etc/nginx/sites-available/default > /dev/null 2>&1
 sudo mv default /etc/nginx/sites-available/
-
-# Prepare transi site.
-git clone https://github.com/gyKa/transi.git
-wget https://raw.githubusercontent.com/gyKa/setup/master/raspberrypi/etc/nginx/sites-available/transi_site
-sudo mv transi_site /etc/nginx/sites-available/
-sudo ln -s /etc/nginx/sites-available/transi_site /etc/nginx/sites-enabled/transi_site
-make install -C transi/
-
-# Prepare pi site.
-wget https://raw.githubusercontent.com/gyKa/setup/master/raspberrypi/etc/nginx/sites-available/pi_site
-sudo mv pi_site /etc/nginx/sites-available/
-sudo ln -s /etc/nginx/sites-available/pi_site /etc/nginx/sites-enabled/pi_site
-
-# Prepare homino project.
-git clone https://github.com/gyKa/homino.git
-make install -C homino/
-
-# Restart PHP5 FastCGI process manager and nginx.
 sudo service php5-fpm restart
 sudo service nginx restart
 
-# Prepare SSH for deployment.
-mkdir .ssh
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQClUVpqPk+DRJxbw18T1Xo7HQUOyYP+s4uahOEuZ8eMb9A540IQaZQcwH0UzICrvOl8gxTDabvOacY8VkLanX1lmkJWYg3B4jbpIY6wBx0H6l8WzHXXpR7GNQpNW7mmOYFB1xztKUIcTURXNozxi/Oeq/5MJJ4rSJOAleOX3MrzaapTkpEas8+Qv/kIJFTNx1yg+gWATG0DQREnekTm45U/2fSCsidt0pRWutyhnxRqpTw7IO1UsuzhFxPWZOxa/tP5DtfqqF2nQUV5zCZKv4T6ng8th1tq9dnmbql1KPmB4YMFrmHMH5CzU4WkvA3u8V76Xsm0bHV6ETt+9HZRIwGr Codeship/gyKa/transi" >> .ssh/authorized_keys
-chmod 700 .ssh
-chmod 600 .ssh/authorized_keys
+# Setup sites.
+git clone https://github.com/gyKa/evenite.git /var/www/evenite
+
+# Setup GIT.
+git config --global user.email "gytis@karciauskas.lt"
+git config --global user.name "Gytis Karčiauskas"
--- vmware/setup.sh
+++ vmware/setup.sh
@@ -61,3 +61,23 @@ sudo pip install bottle
 
 echo "===> Installing VirtualEnv..."
 sudo pip install virtualenv
+
+echo "===> Installing the stable release version of RVM..."
+gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
+\curl -sSL https://get.rvm.io | bash -s stable
+source ~/.rvm/scripts/rvm
+
+echo "===> Installing Ruby..."
+rvm install 2.1.4
+
+echo "===> Installing IRB..."
+sudo apt-get install -y irb
+
+echo "===> Installing NodeJS..."
+sudo apt-get install -y nodejs
+
+echo "===> Installing Rails..."
+gem install rails
+
+echo "===> Install Heroku Toolbelt"
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh

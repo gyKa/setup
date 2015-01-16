5371c70cee486f31e2d555366b8708f357d88445
Gytis Karčiauskas <gytis@karciauskas.lt>
2015-01-16 07:18:06 +0200

    Remove useless stuff.

--- vmware/setup.sh
+++ vmware/setup.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+echo "==> Removing useless stuff..."
+rm ~/examples.desktop
+sudo rm -rf /usr/share/example-content/
+
 echo "==> Downloading the package lists from the repositories and updating them to get information on the newest versions of packages and their dependencies..."
 sudo apt-get update > /dev/null
 

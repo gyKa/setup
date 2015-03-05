5e6a42d4e8fb4b888264e5d204b63e2479bc26b0
Gytis Karčiauskas <gytis@karciauskas.lt>
2015-03-05 22:43:06 +0200

    Setup for NUC.

---
+++ nuc/setup.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get dist-upgrade
+
+sudo apt-get install htop keepassx flashplugin-installer git vim gitg
+
+sudo sh -c "echo 'deb http://download.opensuse.org/repositories/isv:/ownCloud:/desktop/xUbuntu_14.04/ /' >> /etc/apt/sources.list.d/owncloud-client.list"
+wget http://download.opensuse.org/repositories/isv:ownCloud:desktop/xUbuntu_14.04/Release.key
+sudo apt-key add - < Release.key
+rm Release.key
+sudo apt-get update
+sudo apt-get install owncloud-client
+
+ssh-keygen -t rsa -C "NUC"
+
+mkdir ~/Workspace
+
+git config --global user.name "Gytis Karčiauskas"
+git config --global user.email gytis@karciauskas.lt
+

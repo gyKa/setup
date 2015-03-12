f2c5308fc4e2df2932b06c6061e7b8d502eeea55
Gytis Karčiauskas <gytis@karciauskas.lt>
2015-03-12 21:59:54 +0200

    Update setup.

---
+++ nuc/README.md
@@ -0,0 +1,9 @@
+## Installation
+
+Works on Ubuntu 14.04.2 LTS
+
+`wget https://raw.githubusercontent.com/gyKa/setup/master/nuc/setup.sh`
+
+`chmod +x setup.sh`
+
+`bash setup.sh`
--- nuc/setup.sh
+++ nuc/setup.sh
@@ -1,27 +1,39 @@
 #!/bin/sh
 
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get dist-upgrade
+echo "==> Updating the list of packages..."
+sudo apt-get update -y > /dev/null
 
-sudo apt-get install htop keepassx flashplugin-installer git vim gitg \
-python-virtualenv tree mysql-workbench mysql-server libmysqlclient-dev \
-python-dev python3.4-dev
+echo "==> Installing the newest versions of all packages..."
+sudo apt-get upgrade -y > /dev/null
 
+echo "==> Installing ownCloud client..."
 sudo sh -c "echo 'deb http://download.opensuse.org/repositories/isv:/ownCloud:/desktop/xUbuntu_14.04/ /' >> /etc/apt/sources.list.d/owncloud-client.list"
 wget http://download.opensuse.org/repositories/isv:ownCloud:desktop/xUbuntu_14.04/Release.key
 sudo apt-key add - < Release.key
 rm Release.key
-sudo apt-get update
-sudo apt-get install owncloud-client
+sudo apt-get update -y > /dev/null
+sudo apt-get install -y owncloud-client > /dev/null
 
-ssh-keygen -t rsa -C "NUC"
+echo "==> Installing KeePassX..."
+sudo apt-get install -y keepassx > /dev/null
 
-mkdir ~/Workspace
-
-rm ~/examples.desktop
-sudo rm -rf /usr/share/example-content/
-
+echo "==> Installing and setting up Git..."
+sudo apt-get install -y git > /dev/null
 git config --global user.name "Gytis Karčiauskas"
 git config --global user.email gytis@karciauskas.lt
 
+echo "==> Installing GitG..."
+sudo apt-get install -y gitg > /dev/null
+
+echo "==> Generating SSH keys..."
+ssh-keygen -t rsa -C "NUC"
+
+read -p "Upload public SSH key to GitHub.com and press [Enter] key to continue..."
+
+echo "==> Removing example content..."
+rm ~/examples.desktop
+sudo rm -rf /usr/share/example-content/
+
+echo "==> Preparing workspace..."
+mkdir ~/Workspace
+git clone git@github.com:gyKa/setup.git ~/Workspace/setup
